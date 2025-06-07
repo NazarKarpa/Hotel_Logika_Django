dcede1767e0fae75341a7a9acf8f91a92043456c
nazar <Bonasane@gmail.com>
2025-06-07 12:32:06 +0000

    update admin panel

--- reservation/admin.py
+++ reservation/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 
 from reservation.models import Room, Reservation, TypeRoom
 
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('room', 'date_start', 'date_end', 'phone_number')
+    search_fields = ('date_start', 'phone_number')
+
+
+
 admin.site.register(Room)
-admin.site.register(Reservation)
+admin.site.register(Reservation, ReservationAdmin)
 admin.site.register(TypeRoom)
