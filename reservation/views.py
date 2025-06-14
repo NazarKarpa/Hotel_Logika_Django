029d54cbc1ef374a108c185e0f42102dd90f9634
Nazar Karpa <Bonasane@gmail.com>
2025-06-14 12:33:24 +0300

    add html and css

--- reservation/views.py
+++ reservation/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from reservation.models import Room, Reservation, TypeRoom
+
+
+def room_list(request):
+    rooms = Room.objects.all()
+
+    context = {
+        'rooms': rooms
+
+    }
+
+    return render(request, template_name='room_list.html', context=context)
